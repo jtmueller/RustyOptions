3a2dfcf7e7b9082c31bbda0c57c45b5066ebed92
Joel Mueller <joel.mueller@gmail.com>
2023-01-09 17:21:17 -0600

    Option MapOrElseAsync

--- src/RustyOptions.Tests/OptionAsyncTests.cs
+++ src/RustyOptions.Tests/OptionAsyncTests.cs
@@ -146,4 +146,80 @@ public sealed class OptionAsyncTests
         yield return new object[] { taskSrcNone, valTaskMapper, defaultValue, expectedNone };
         yield return new object[] { valTaskSrcNone, valTaskMapper, defaultValue, expectedNone };
     }
+
+    [Theory]
+    [MemberData(nameof(GetMapOrElseAsyncValues))]
+    public async Task CanMapOrElseAsync(object source, object mapper, object defaultFactory, long expected)
+    {
+        switch ((source, mapper, defaultFactory))
+        {
+            case (Option<int> src, Func<int, ValueTask<long>> mpr, Func<ValueTask<long>> dff):
+                Assert.Equal(expected, await src.MapOrElseAsync(mpr, dff));
+                break;
+            case (Option<int> src, Func<int, Task<long>> mpr, Func<Task<long>> dff):
+                Assert.Equal(expected, await src.MapOrElseAsync(mpr, dff));
+                break;
+            case (ValueTask<Option<int>> src, Func<int, long> mpr, Func<long> dff):
+                Assert.Equal(expected, await src.MapOrElseAsync(mpr, dff));
+                break;
+            case (Task<Option<int>> src, Func<int, long> mpr, Func<long> dff):
+                Assert.Equal(expected, await src.MapOrElseAsync(mpr, dff));
+                break;
+            case (ValueTask<Option<int>> src, Func<int, ValueTask<long>> mpr, Func<ValueTask<long>> dff):
+                Assert.Equal(expected, await src.MapOrElseAsync(mpr, dff));
+                break;
+            case (Task<Option<int>> src, Func<int, ValueTask<long>> mpr, Func<ValueTask<long>> dff):
+                Assert.Equal(expected, await src.MapOrElseAsync(mpr, dff));
+                break;
+            case (ValueTask<Option<int>> src, Func<int, Task<long>> mpr, Func<Task<long>> dff):
+                Assert.Equal(expected, await src.MapOrElseAsync(mpr, dff));
+                break;
+            case (Task<Option<int>> src, Func<int, Task<long>> mpr, Func<Task<long>> dff):
+                Assert.Equal(expected, await src.MapOrElseAsync(mpr, dff));
+                break;
+
+            default:
+                Assert.Fail($"Unexpected source/mapper: {source.GetType().Name}/{mapper.GetType().Name}");
+                break;
+        }
+    }
+
+    public static IEnumerable<object[]> GetMapOrElseAsyncValues()
+    {
+        var defaultFactory = () => -1L;
+        var taskDefault = () => Task.FromResult(-1L);
+        var valueTaskDefault = () => ValueTask.FromResult(-1L);
+
+        var expected = 84L;
+        var expectedNone = -1L;
+
+        var source = Some(42);
+        var sourceNone = None<int>();
+        var taskSrc = Task.FromResult(source);
+        var valTaskSrc = ValueTask.FromResult(source);
+        var taskSrcNone = Task.FromResult(sourceNone);
+        var valTaskSrcNone = ValueTask.FromResult(sourceNone);
+
+        var mapper = (int x) => (long)(x * 2);
+        var taskMapper = (int x) => Task.FromResult((long)(x * 2));
+        var valTaskMapper = (int x) => ValueTask.FromResult((long)(x * 2));
+
+        yield return new object[] { source, taskMapper, taskDefault, expected };
+        yield return new object[] { source, valTaskMapper, valueTaskDefault, expected };
+        yield return new object[] { taskSrc, mapper, defaultFactory, expected };
+        yield return new object[] { valTaskSrc, mapper, defaultFactory, expected };
+        yield return new object[] { taskSrc, taskMapper, taskDefault, expected };
+        yield return new object[] { valTaskSrc, taskMapper, taskDefault, expected };
+        yield return new object[] { taskSrc, valTaskMapper, valueTaskDefault, expected };
+        yield return new object[] { valTaskSrc, valTaskMapper, valueTaskDefault, expected };
+
+        yield return new object[] { sourceNone, taskMapper, taskDefault, expectedNone };
+        yield return new object[] { sourceNone, valTaskMapper, valueTaskDefault, expectedNone };
+        yield return new object[] { taskSrcNone, mapper, defaultFactory, expectedNone };
+        yield return new object[] { valTaskSrcNone, mapper, defaultFactory, expectedNone };
+        yield return new object[] { taskSrcNone, taskMapper, taskDefault, expectedNone };
+        yield return new object[] { valTaskSrcNone, taskMapper, taskDefault, expectedNone };
+        yield return new object[] { taskSrcNone, valTaskMapper, valueTaskDefault, expectedNone };
+        yield return new object[] { valTaskSrcNone, valTaskMapper, valueTaskDefault, expectedNone };
+    }
 }
--- src/RustyOptions/OptionAsyncExtensions.cs
+++ src/RustyOptions/OptionAsyncExtensions.cs
@@ -1,4 +1,5 @@
-﻿using static System.ArgumentNullException;
+﻿using System.Runtime.CompilerServices;
+using static System.ArgumentNullException;
 using static RustyOptions.Option;
 
 namespace RustyOptions;
@@ -186,7 +187,7 @@ public static class OptionAsyncExtensions
     /// Asynchronously returns the provided default result (if <c>None</c>), or applies a function to the contained value (if <c>Some</c>).
     /// <para>
     ///   Arguments passed to this method are eagerly evaluated; if you are passing the result of a function call,
-    ///   it is recommended to use <see cref="MapOrElseAsync"/>, which is lazily evaluated.</para>
+    ///   it is recommended to use <see cref="MapOrElseAsync{T1, T2}(Option{T1}, Func{T1, ValueTask{T2}}, Func{ValueTask{T2}})"/>, which is lazily evaluated.</para>
     /// </summary>
     /// <typeparam name="T1">The type of the option's value.</typeparam>
     /// <typeparam name="T2">The return type after mapping.</typeparam>
@@ -209,7 +210,7 @@ public static class OptionAsyncExtensions
     /// Asynchronously returns the provided default result (if <c>None</c>), or applies a function to the contained value (if <c>Some</c>).
     /// <para>
     ///   Arguments passed to this method are eagerly evaluated; if you are passing the result of a function call,
-    ///   it is recommended to use <see cref="MapOrElseAsync"/>, which is lazily evaluated.</para>
+    ///   it is recommended to use <see cref="MapOrElseAsync{T1, T2}(Option{T1}, Func{T1, Task{T2}}, Func{Task{T2}})"/>, which is lazily evaluated.</para>
     /// </summary>
     /// <typeparam name="T1">The type of the option's value.</typeparam>
     /// <typeparam name="T2">The return type after mapping.</typeparam>
@@ -232,7 +233,7 @@ public static class OptionAsyncExtensions
     /// Asynchronously returns the provided default result (if <c>None</c>), or applies a function to the contained value (if <c>Some</c>).
     /// <para>
     ///   Arguments passed to this method are eagerly evaluated; if you are passing the result of a function call,
-    ///   it is recommended to use <see cref="MapOrElseAsync"/>, which is lazily evaluated.</para>
+    ///   it is recommended to use <see cref="MapOrElseAsync{T1, T2}(ValueTask{Option{T1}}, Func{T1, T2}, Func{T2})"/>, which is lazily evaluated.</para>
     /// </summary>
     /// <typeparam name="T1">The type of the option's value.</typeparam>
     /// <typeparam name="T2">The return type after mapping.</typeparam>
@@ -256,7 +257,7 @@ public static class OptionAsyncExtensions
     /// Asynchronously returns the provided default result (if <c>None</c>), or applies a function to the contained value (if <c>Some</c>).
     /// <para>
     ///   Arguments passed to this method are eagerly evaluated; if you are passing the result of a function call,
-    ///   it is recommended to use <see cref="MapOrElseAsync"/>, which is lazily evaluated.</para>
+    ///   it is recommended to use <see cref="MapOrElseAsync{T1, T2}(Task{Option{T1}}, Func{T1, T2}, Func{T2})"/>, which is lazily evaluated.</para>
     /// </summary>
     /// <typeparam name="T1">The type of the option's value.</typeparam>
     /// <typeparam name="T2">The return type after mapping.</typeparam>
@@ -280,7 +281,7 @@ public static class OptionAsyncExtensions
     /// Asynchronously returns the provided default result (if <c>None</c>), or applies a function to the contained value (if <c>Some</c>).
     /// <para>
     ///   Arguments passed to this method are eagerly evaluated; if you are passing the result of a function call,
-    ///   it is recommended to use <see cref="MapOrElseAsync"/>, which is lazily evaluated.</para>
+    ///   it is recommended to use <see cref="MapOrElseAsync{T1, T2}(ValueTask{Option{T1}}, Func{T1, ValueTask{T2}}, Func{ValueTask{T2}})"/>, which is lazily evaluated.</para>
     /// </summary>
     /// <typeparam name="T1">The type of the option's value.</typeparam>
     /// <typeparam name="T2">The return type after mapping.</typeparam>
@@ -305,7 +306,7 @@ public static class OptionAsyncExtensions
     /// Asynchronously returns the provided default result (if <c>None</c>), or applies a function to the contained value (if <c>Some</c>).
     /// <para>
     ///   Arguments passed to this method are eagerly evaluated; if you are passing the result of a function call,
-    ///   it is recommended to use <see cref="MapOrElseAsync"/>, which is lazily evaluated.</para>
+    ///   it is recommended to use <see cref="MapOrElseAsync{T1, T2}(Task{Option{T1}}, Func{T1, ValueTask{T2}}, Func{ValueTask{T2}})"/>, which is lazily evaluated.</para>
     /// </summary>
     /// <typeparam name="T1">The type of the option's value.</typeparam>
     /// <typeparam name="T2">The return type after mapping.</typeparam>
@@ -330,7 +331,7 @@ public static class OptionAsyncExtensions
     /// Asynchronously returns the provided default result (if <c>None</c>), or applies a function to the contained value (if <c>Some</c>).
     /// <para>
     ///   Arguments passed to this method are eagerly evaluated; if you are passing the result of a function call,
-    ///   it is recommended to use <see cref="MapOrElseAsync"/>, which is lazily evaluated.</para>
+    ///   it is recommended to use <see cref="MapOrElseAsync{T1, T2}(ValueTask{Option{T1}}, Func{T1, Task{T2}}, Func{Task{T2}})"/>, which is lazily evaluated.</para>
     /// </summary>
     /// <typeparam name="T1">The type of the option's value.</typeparam>
     /// <typeparam name="T2">The return type after mapping.</typeparam>
@@ -355,7 +356,7 @@ public static class OptionAsyncExtensions
     /// Asynchronously returns the provided default result (if <c>None</c>), or applies a function to the contained value (if <c>Some</c>).
     /// <para>
     ///   Arguments passed to this method are eagerly evaluated; if you are passing the result of a function call,
-    ///   it is recommended to use <see cref="MapOrElseAsync"/>, which is lazily evaluated.</para>
+    ///   it is recommended to use <see cref="MapOrElseAsync{T1, T2}(Task{Option{T1}}, Func{T1, Task{T2}}, Func{Task{T2}})"/>, which is lazily evaluated.</para>
     /// </summary>
     /// <typeparam name="T1">The type of the option's value.</typeparam>
     /// <typeparam name="T2">The return type after mapping.</typeparam>
@@ -374,5 +375,148 @@ public static class OptionAsyncExtensions
         return opt.IsSome(out var value)
             ? await mapper(value).ConfigureAwait(false)
             : defaultValue;
+    }
+
+    /// <summary>
+    /// Asynchronously computes a default function result (if <c>None</c>), or applies a different function to the contained value (if <c>Some</c>).
+    /// </summary>
+    /// <typeparam name="T1">The type of the option's value.</typeparam>
+    /// <typeparam name="T2">The return type after mapping.</typeparam>
+    /// <param name="self">The option to map.</param>
+    /// <param name="mapper">The function that maps the value contained in the option.</param>
+    /// <param name="defaultFactory">The function that lazily generates a default value, if required.</param>
+    /// <returns>The mapped value, or the default value.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="mapper"/> or <paramref name="defaultFactory"/> is null.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ValueTask<T2> MapOrElseAsync<T1, T2>(this Option<T1> self, Func<T1, ValueTask<T2>> mapper, Func<ValueTask<T2>> defaultFactory)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        return self.Match(mapper, defaultFactory);
+    }
+
+    /// <summary>
+    /// Asynchronously computes a default function result (if <c>None</c>), or applies a different function to the contained value (if <c>Some</c>).
+    /// </summary>
+    /// <typeparam name="T1">The type of the option's value.</typeparam>
+    /// <typeparam name="T2">The return type after mapping.</typeparam>
+    /// <param name="self">The option to map.</param>
+    /// <param name="mapper">The function that maps the value contained in the option.</param>
+    /// <param name="defaultFactory">The function that lazily generates a default value, if required.</param>
+    /// <returns>The mapped value, or the default value.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="mapper"/> or <paramref name="defaultFactory"/> is null.</exception>
+    public static async ValueTask<T2> MapOrElseAsync<T1, T2>(this Option<T1> self, Func<T1, Task<T2>> mapper, Func<Task<T2>> defaultFactory)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        return await self.Match(mapper, defaultFactory);
+    }
+
+    /// <summary>
+    /// Asynchronously computes a default function result (if <c>None</c>), or applies a different function to the contained value (if <c>Some</c>).
+    /// </summary>
+    /// <typeparam name="T1">The type of the option's value.</typeparam>
+    /// <typeparam name="T2">The return type after mapping.</typeparam>
+    /// <param name="self">The option to map.</param>
+    /// <param name="mapper">The function that maps the value contained in the option.</param>
+    /// <param name="defaultFactory">The function that lazily generates a default value, if required.</param>
+    /// <returns>The mapped value, or the default value.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="mapper"/> or <paramref name="defaultFactory"/> is null.</exception>
+    public static async ValueTask<T2> MapOrElseAsync<T1, T2>(this ValueTask<Option<T1>> self, Func<T1, T2> mapper, Func<T2> defaultFactory)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        var opt = await self.ConfigureAwait(false);
+        return opt.Match(mapper, defaultFactory);
+    }
+
+    /// <summary>
+    /// Asynchronously computes a default function result (if <c>None</c>), or applies a different function to the contained value (if <c>Some</c>).
+    /// </summary>
+    /// <typeparam name="T1">The type of the option's value.</typeparam>
+    /// <typeparam name="T2">The return type after mapping.</typeparam>
+    /// <param name="self">The option to map.</param>
+    /// <param name="mapper">The function that maps the value contained in the option.</param>
+    /// <param name="defaultFactory">The function that lazily generates a default value, if required.</param>
+    /// <returns>The mapped value, or the default value.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="mapper"/> or <paramref name="defaultFactory"/> is null.</exception>
+    public static async ValueTask<T2> MapOrElseAsync<T1, T2>(this Task<Option<T1>> self, Func<T1, T2> mapper, Func<T2> defaultFactory)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        var opt = await self.ConfigureAwait(false);
+        return opt.Match(mapper, defaultFactory);
+    }
+
+    /// <summary>
+    /// Asynchronously computes a default function result (if <c>None</c>), or applies a different function to the contained value (if <c>Some</c>).
+    /// </summary>
+    /// <typeparam name="T1">The type of the option's value.</typeparam>
+    /// <typeparam name="T2">The return type after mapping.</typeparam>
+    /// <param name="self">The option to map.</param>
+    /// <param name="mapper">The function that maps the value contained in the option.</param>
+    /// <param name="defaultFactory">The function that lazily generates a default value, if required.</param>
+    /// <returns>The mapped value, or the default value.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="mapper"/> or <paramref name="defaultFactory"/> is null.</exception>
+    public static async ValueTask<T2> MapOrElseAsync<T1, T2>(this ValueTask<Option<T1>> self, Func<T1, ValueTask<T2>> mapper, Func<ValueTask<T2>> defaultFactory)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        var opt = await self.ConfigureAwait(false);
+        return await opt.Match(mapper, defaultFactory);
+    }
+
+    /// <summary>
+    /// Asynchronously computes a default function result (if <c>None</c>), or applies a different function to the contained value (if <c>Some</c>).
+    /// </summary>
+    /// <typeparam name="T1">The type of the option's value.</typeparam>
+    /// <typeparam name="T2">The return type after mapping.</typeparam>
+    /// <param name="self">The option to map.</param>
+    /// <param name="mapper">The function that maps the value contained in the option.</param>
+    /// <param name="defaultFactory">The function that lazily generates a default value, if required.</param>
+    /// <returns>The mapped value, or the default value.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="mapper"/> or <paramref name="defaultFactory"/> is null.</exception>
+    public static async ValueTask<T2> MapOrElseAsync<T1, T2>(this Task<Option<T1>> self, Func<T1, ValueTask<T2>> mapper, Func<ValueTask<T2>> defaultFactory)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        var opt = await self.ConfigureAwait(false);
+        return await opt.Match(mapper, defaultFactory);
+    }
+
+    /// <summary>
+    /// Asynchronously computes a default function result (if <c>None</c>), or applies a different function to the contained value (if <c>Some</c>).
+    /// </summary>
+    /// <typeparam name="T1">The type of the option's value.</typeparam>
+    /// <typeparam name="T2">The return type after mapping.</typeparam>
+    /// <param name="self">The option to map.</param>
+    /// <param name="mapper">The function that maps the value contained in the option.</param>
+    /// <param name="defaultFactory">The function that lazily generates a default value, if required.</param>
+    /// <returns>The mapped value, or the default value.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="mapper"/> or <paramref name="defaultFactory"/> is null.</exception>
+    public static async ValueTask<T2> MapOrElseAsync<T1, T2>(this ValueTask<Option<T1>> self, Func<T1, Task<T2>> mapper, Func<Task<T2>> defaultFactory)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        var opt = await self.ConfigureAwait(false);
+        return await opt.Match(mapper, defaultFactory);
+    }
+
+    /// <summary>
+    /// Asynchronously computes a default function result (if <c>None</c>), or applies a different function to the contained value (if <c>Some</c>).
+    /// </summary>
+    /// <typeparam name="T1">The type of the option's value.</typeparam>
+    /// <typeparam name="T2">The return type after mapping.</typeparam>
+    /// <param name="self">The option to map.</param>
+    /// <param name="mapper">The function that maps the value contained in the option.</param>
+    /// <param name="defaultFactory">The function that lazily generates a default value, if required.</param>
+    /// <returns>The mapped value, or the default value.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="mapper"/> or <paramref name="defaultFactory"/> is null.</exception>
+    public static async ValueTask<T2> MapOrElseAsync<T1, T2>(this Task<Option<T1>> self, Func<T1, Task<T2>> mapper, Func<Task<T2>> defaultFactory)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        var opt = await self.ConfigureAwait(false);
+        return await opt.Match(mapper, defaultFactory);
     }
 }
