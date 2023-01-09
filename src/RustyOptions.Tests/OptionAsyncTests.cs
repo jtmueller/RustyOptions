b6ea8b85611eb3655a96d43ad3da01b65cb99299
Joel Mueller <joel.mueller@gmail.com>
2023-01-09 15:18:22 -0600

    Option MapAsync coverage

--- src/RustyOptions.Tests/OptionAsyncTests.cs
+++ src/RustyOptions.Tests/OptionAsyncTests.cs
@@ -44,9 +44,13 @@ public sealed class OptionAsyncTests
     public static IEnumerable<object[]> GetMapAsyncValues()
     {
         var expected = Some(84L);
+        var expectedNone = None<long>();
         var source = Some(42);
+        var sourceNone = None<int>();
         var taskSrc = Task.FromResult(source);
         var valTaskSrc = ValueTask.FromResult(source);
+        var taskSrcNone = Task.FromResult(sourceNone);
+        var valTaskSrcNone = ValueTask.FromResult(sourceNone);
 
         var mapper = (int x) => (long)(x * 2);
         var taskMapper = (int x) => Task.FromResult((long)(x * 2));
@@ -60,5 +64,14 @@ public sealed class OptionAsyncTests
         yield return new object[] { valTaskSrc, taskMapper, expected };
         yield return new object[] { taskSrc, valTaskMapper, expected };
         yield return new object[] { valTaskSrc, valTaskMapper, expected };
+
+        yield return new object[] { sourceNone, taskMapper, expectedNone };
+        yield return new object[] { sourceNone, valTaskMapper, expectedNone };
+        yield return new object[] { taskSrcNone, mapper, expectedNone };
+        yield return new object[] { valTaskSrcNone, mapper, expectedNone };
+        yield return new object[] { taskSrcNone, taskMapper, expectedNone };
+        yield return new object[] { valTaskSrcNone, taskMapper, expectedNone };
+        yield return new object[] { taskSrcNone, valTaskMapper, expectedNone };
+        yield return new object[] { valTaskSrcNone, valTaskMapper, expectedNone };
     }
 }
