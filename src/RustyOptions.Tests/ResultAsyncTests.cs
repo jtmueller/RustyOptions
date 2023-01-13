8d90a39f4c26a86e43e9d350777d954ceb81b59c
Joel Mueller <joel.mueller@gmail.com>
2023-01-13 16:49:07 -0600

    Result MapAsync tests

---
+++ src/RustyOptions.Tests/ResultAsyncTests.cs
@@ -0,0 +1,79 @@
+﻿using RustyOptions.Async;
+using static RustyOptions.Result;
+
+namespace RustyOptions.Tests;
+
+public class ResultAsyncTests
+{
+    [Theory]
+    [MemberData(nameof(GetMapAsyncValues))]
+    public async Task CanMapAsync(object source, object mapper, Result<long, string> expected)
+    {
+        switch ((source, mapper))
+        {
+            case (Result<int, string> src, Func<int, ValueTask<long>> mpr):
+                Assert.Equal(expected, await src.MapAsync(mpr));
+                break;
+            case (Result<int, string> src, Func<int, Task<long>> mpr):
+                Assert.Equal(expected, await src.MapAsync(mpr));
+                break;
+            case (ValueTask<Result<int, string>> src, Func<int, long> mpr):
+                Assert.Equal(expected, await src.MapAsync(mpr));
+                break;
+            case (Task<Result<int, string>> src, Func<int, long> mpr):
+                Assert.Equal(expected, await src.MapAsync(mpr));
+                break;
+            case (ValueTask<Result<int, string>> src, Func<int, ValueTask<long>> mpr):
+                Assert.Equal(expected, await src.MapAsync(mpr));
+                break;
+            case (Task<Result<int, string>> src, Func<int, ValueTask<long>> mpr):
+                Assert.Equal(expected, await src.MapAsync(mpr));
+                break;
+            case (ValueTask<Result<int, string>> src, Func<int, Task<long>> mpr):
+                Assert.Equal(expected, await src.MapAsync(mpr));
+                break;
+            case (Task<Result<int, string>> src, Func<int, Task<long>> mpr):
+                Assert.Equal(expected, await src.MapAsync(mpr));
+                break;
+
+            default:
+                Assert.Fail($"Unexpected source/mapper: {source.GetType().Name}/{mapper.GetType().Name}");
+                break;
+        }
+    }
+
+    public static IEnumerable<object[]> GetMapAsyncValues()
+    {
+        var expected = Ok(84L);
+        var expectedNone = Err<long>("no value");
+        var source = Ok(42);
+        var sourceNone = Err<int>("no value");
+        var taskSrc = Task.FromResult(source);
+        var valTaskSrc = ValueTask.FromResult(source);
+        var taskSrcNone = Task.FromResult(sourceNone);
+        var valTaskSrcNone = ValueTask.FromResult(sourceNone);
+
+        var mapper = (int x) => (long)(x * 2);
+        var taskMapper = (int x) => Task.FromResult((long)(x * 2));
+        var valTaskMapper = (int x) => ValueTask.FromResult((long)(x * 2));
+
+        yield return new object[] { source, taskMapper, expected };
+        yield return new object[] { source, valTaskMapper, expected };
+        yield return new object[] { taskSrc, mapper, expected };
+        yield return new object[] { valTaskSrc, mapper, expected };
+        yield return new object[] { taskSrc, taskMapper, expected };
+        yield return new object[] { valTaskSrc, taskMapper, expected };
+        yield return new object[] { taskSrc, valTaskMapper, expected };
+        yield return new object[] { valTaskSrc, valTaskMapper, expected };
+
+        yield return new object[] { sourceNone, taskMapper, expectedNone };
+        yield return new object[] { sourceNone, valTaskMapper, expectedNone };
+        yield return new object[] { taskSrcNone, mapper, expectedNone };
+        yield return new object[] { valTaskSrcNone, mapper, expectedNone };
+        yield return new object[] { taskSrcNone, taskMapper, expectedNone };
+        yield return new object[] { valTaskSrcNone, taskMapper, expectedNone };
+        yield return new object[] { taskSrcNone, valTaskMapper, expectedNone };
+        yield return new object[] { valTaskSrcNone, valTaskMapper, expectedNone };
+    }
+}
+
